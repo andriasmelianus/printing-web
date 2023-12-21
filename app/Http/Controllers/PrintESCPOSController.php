6d87f1cd60e6a110928d9bfb8d52b5c99fbfbc6a
Andrias Melianus Siwabessy <andriasmelianus@gmail.com>
2023-12-21 11:42:12 +0700

    Add Escp2 class usage example.

--- app/Http/Controllers/PrintESCPOSController.php
+++ app/Http/Controllers/PrintESCPOSController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\WebClientPrint\Escp2;
 use Illuminate\Http\Request;
 
 
@@ -30,67 +31,135 @@ class PrintESCPOSController extends Controller
     public const ESC = '0x1B';
     public const LF = '0x0A';
     public const RESET = '@';
+    public const TYPEFACE = '0x6B';
 
+    // Initialization
     public const ESC_INITIALIZE = self::ESC . self::RESET;
+    public const SETUP_UNIT = self::ESC . '0x28' . '0x55' . '0x01' . '0x00' . '0x0A';
+    // Page setup
+    public const SETUP_PAGE_LENGTH = self::ESC . '0x28' . '0x43' . '0x02' . '0x00' . '0x78' . '0x0F';
+    // LINE SPACING
+    public const SPACING_1_8 = self::ESC . '0x30';
+    public const SPACING_1_6 = self::ESC . '0x32';
+    // TABS
+    public const TAB_SETUP = self::ESC . '0x44';
+    public const TAB_CLEAR = self::ESC . '0x00';
+    // PROPORTIONAL (Non-monospaced)
+    public const FONT_PROPORTIONAL_START = self::ESC . '0x70' . '0x01';
+    public const FONT_PROPORTIONAL_END = self::ESC . '0x70' . '0x00';
+    // CONDENSED
+    public const FONT_CONDENSED_ON = self::ESC . '0x0F';
+    public const FONT_CONDENSED_OFF = self::ESC . '0x12';
     // BOLD
     public const FONT_BOLD_START = self::ESC . 'E';
     public const FONT_BOLD_END = self::ESC . 'F';
     // ITALIC
     public const FONT_ITALIC_START = self::ESC . '4';
     public const FONT_ITALIC_END = self::ESC . '5';
-    // PROPORTIONAL (Non-monospaced)
-    public const FONT_PROPORTIONAL_START = self::ESC . 'p1';
-    public const FONT_PROPORTIONAL_END = self::ESC . 'p0';
     // Typeface
-    public const TYPEFACE_ROMAN = self::ESC . 'k0';
-    public const TYPEFACE_SANS_SERIF = self::ESC . 'k1';
-    public const TYPEFACE_COURIER = self::ESC . 'k2';
-    public const TYPEFACE_PRESTIGE = self::ESC . 'k3';
-    public const TYPEFACE_SCRIPT = self::ESC . 'k4';
-    public const TYPEFACE_OCR_B = self::ESC . 'k5';
-    public const TYPEFACE_OCR_A = self::ESC . 'k6';
-    public const TYPEFACE_ORATOR = self::ESC . 'k7';
-    public const TYPEFACE_ORATOR_S = self::ESC . 'k8';
-    public const TYPEFACE_SCRIPT_C = self::ESC . 'k9';
-    public const TYPEFACE_ROMAN_T = self::ESC . 'kA';
-    public const TYPEFACE_SANS_SERIF_H = self::ESC . 'kB';
-    public const TYPEFACE_SV_BUSABA = self::ESC . 'k' . '0x1E';
-    public const TYPEFACE_SV_JITTRA = self::ESC . 'k' . '0x1F';
+    public const TYPEFACE_ROMAN = self::ESC . self::TYPEFACE . '0x00';
+    public const TYPEFACE_SANS_SERIF = self::ESC . self::TYPEFACE . '0x01';
+    public const TYPEFACE_COURIER = self::ESC . self::TYPEFACE . '0x02';
+    public const TYPEFACE_PRESTIGE = self::ESC . self::TYPEFACE . '0x03';
+    public const TYPEFACE_SCRIPT = self::ESC . self::TYPEFACE . '0x04';
+    public const TYPEFACE_OCR_B = self::ESC . self::TYPEFACE . '0x05';
+    public const TYPEFACE_OCR_A = self::ESC . self::TYPEFACE . '0x06';
+    public const TYPEFACE_ORATOR = self::ESC . self::TYPEFACE . '0x07';
+    public const TYPEFACE_ORATOR_S = self::ESC . self::TYPEFACE . '0x08';
+    public const TYPEFACE_SCRIPT_C = self::ESC . self::TYPEFACE . '0x09';
+    public const TYPEFACE_ROMAN_T = self::ESC . self::TYPEFACE . '0x0A';
+    public const TYPEFACE_SANS_SERIF_H = self::ESC . self::TYPEFACE . '0x0B';
+    public const TYPEFACE_SV_BUSABA = self::ESC . self::TYPEFACE . '0x1E';
+    public const TYPEFACE_SV_JITTRA = self::ESC . self::TYPEFACE . '0x1F';
+    // User-defined characters
+    public const USER_DEFINED_CHARACTERS_ON = self::ESC . '0x25' . '0x01';
+    public const USER_DEFINED_CHARACTERS_OFF = self::ESC . '0x25' . '0x00';
+    // Custom pitch & point
+    // 18 characters per inch,
+    public const CUSTOM_PITCH_POINT = self::ESC . '0x58' . '0x14' . '0x15' . '0x00';
+
+    // Mechanical control
+    // public const PAPER_LOAD_EJECT = self::ESC . '0x19' . '0x31';
 
     public function printCommands(Request $request)
     {
-
         if ($request->exists(WebClientPrint::CLIENT_PRINT_JOB)) {
-
             $useDefaultPrinter = ($request->input('useDefaultPrinter') === 'checked');
             $printerName = urldecode($request->input('printerName'));
 
-            $cmds = self::ESC_INITIALIZE;
-            $cmds .= self::ESC . '0'; // 1/8-inch line spacing
-            $cmds .= self::ESC . 'g'; // Font 15 cpi
+            $printer = new Escp2();
+            $cmds = $printer->initializePrinter()
+                ->addBoldText('Surabaya PANAS SEKALI', true)
+                ->addItalicText('Malang juga PANAS', true)
+                ->generate();
 
-            $cmds .= self::FONT_PROPORTIONAL_START;
-            $cmds .= 'NPWP: 01.682.572.1-641.000, Ijin PBF: FP.1293819' . self::LF;
-            $cmds .= 'Alamat: JL. RAYA TAMAN 48A' . self::LF;
-            $cmds .= self::FONT_BOLD_START . 'Alamat: JL. RAYA TAMAN 48A' . self::FONT_BOLD_END . self::LF;
-            $cmds .= self::FONT_ITALIC_START . 'Alamat: JL. RAYA TAMAN 48A' . self::FONT_ITALIC_END . self::LF;
-            // Non-monospaced
-            // Typeface variations
-            $cmds .= self::TYPEFACE_SANS_SERIF;
-            $cmds .= 'Alamat: JL. RAYA TAMAN 48A'  . self::LF;
-            $cmds .= self::TYPEFACE_COURIER;
-            $cmds .= 'Alamat: JL. RAYA TAMAN 48A' . self::LF;
-            $cmds .= self::TYPEFACE_PRESTIGE;
-            $cmds .= 'Alamat: JL. RAYA TAMAN 48A' . self::LF;
-            $cmds .= self::TYPEFACE_SCRIPT;
-            $cmds .= 'Alamat: JL. RAYA TAMAN 48A' . self::LF;
-            $cmds .= self::FONT_PROPORTIONAL_END;
-            // Monospaced Roman
-            $cmds .= self::TYPEFACE_ROMAN;
-            $cmds .= 'Alamat: JL. RAYA TAMAN 48A' . self::LF;
-            $cmds .= 'Alamat: JL. RAYA TAMAN 48A' . self::LF;
-            $cmds .= 'Alamat: JL. RAYA TAMAN 48A' . self::LF;
-            $cmds .= 'Alamat: JL. RAYA TAMAN 48A' . self::LF;
+            // $cmds = self::ESC_INITIALIZE;
+            // $cmds .= 'Initialize printer...' . self::LF;
+
+            // $cmds .= self::SETUP_PAGE_LENGTH;
+
+            // $cmds .= self::FONT_PROPORTIONAL_START; // Non-monospaced text
+            // $cmds .= 'Text with proportional on' . self::LF;
+
+            // $cmds .= self::SPACING_1_8; // 1/8-inch line spacing
+            // $cmds .= 'Spacing 1/8 selected' . self::LF;
+
+            // $cmds .= self::FONT_BOLD_START . 'Bold text' . self::FONT_BOLD_END . self::LF;
+
+            // $cmds .= self::FONT_ITALIC_START . 'Italic text' . self::FONT_ITALIC_END . self::LF;
+
+            // $cmds .= self::USER_DEFINED_CHARACTERS_OFF;
+            // $cmds .= 'User defined switched off.' . self::LF;
+
+            // $cmds .= self::TYPEFACE_SANS_SERIF;
+            // $cmds .= 'User defined switched off. But with sans serif typeface.' . self::LF . self::LF;
+
+            // $cmds .= self::TAB_SETUP . '0x14' . '0x28' . '0x3C' . '0x00';
+            // $cmds .= 'Setting up pre-defined TABs.' . self::LF;
+            // $cmds .= 'First text' . '0x09' . '[TAB] Second text' . '0x09' . '[TAB] Third text' . self::LF;
+            // $cmds .= self::FONT_CONDENSED_ON;
+            // $cmds .= 'Turning on condensed text...' . self::LF;
+            // $cmds .= 'First text' . '0x09' . '[TAB] Second text' . '0x09' . '[TAB] Third text' . self::LF;
+            // $cmds .= self::FONT_CONDENSED_OFF;
+            // $cmds .= 'Turning off condensed text...' . self::LF . self::LF;
+            // $cmds .= 'Setting up PITCH & POINT...' . self::LF;
+            // $cmds .= self::CUSTOM_PITCH_POINT;
+            // $cmds .= 'Untuk informasi, saran, dan keluhan, silakan hubungi Customer Service di 081130582777' . self::LF;
+            // $cmds .= 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dolorum facilis maxime quia, nesciunt, voluptatem excepturi voluptas aut asperiores molestiae, eveniet dolorem nulla neque impedit aperiam possimus voluptatibus quisquam itaque. ';
+            // $cmds .= 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dolorum facilis maxime quia, nesciunt, voluptatem excepturi voluptas aut asperiores molestiae, eveniet dolorem nulla neque impedit aperiam possimus voluptatibus quisquam itaque. ';
+            // $cmds .= 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dolorum facilis maxime quia, nesciunt, voluptatem excepturi voluptas aut asperiores molestiae, eveniet dolorem nulla neque impedit aperiam possimus voluptatibus quisquam itaque. ';
+            // $cmds .= 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dolorum facilis maxime quia, nesciunt, voluptatem excepturi voluptas aut asperiores molestiae, eveniet dolorem nulla neque impedit aperiam possimus voluptatibus quisquam itaque. ';
+            // $cmds .= 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dolorum facilis maxime quia, nesciunt, voluptatem excepturi voluptas aut asperiores molestiae, eveniet dolorem nulla neque impedit aperiam possimus voluptatibus quisquam itaque. ';
+            // $cmds .= 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dolorum facilis maxime quia, nesciunt, voluptatem excepturi voluptas aut asperiores molestiae, eveniet dolorem nulla neque impedit aperiam possimus voluptatibus quisquam itaque. ';
+            // $cmds .= 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dolorum facilis maxime quia, nesciunt, voluptatem excepturi voluptas aut asperiores molestiae, eveniet dolorem nulla neque impedit aperiam possimus voluptatibus quisquam itaque. ';
+            // $cmds .= 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dolorum facilis maxime quia, nesciunt, voluptatem excepturi voluptas aut asperiores molestiae, eveniet dolorem nulla neque impedit aperiam possimus voluptatibus quisquam itaque. ';
+            // $cmds .= 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dolorum facilis maxime quia, nesciunt, voluptatem excepturi voluptas aut asperiores molestiae, eveniet dolorem nulla neque impedit aperiam possimus voluptatibus quisquam itaque. ';
+            // $cmds .= 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dolorum facilis maxime quia, nesciunt, voluptatem excepturi voluptas aut asperiores molestiae, eveniet dolorem nulla neque impedit aperiam possimus voluptatibus quisquam itaque. ';
+            // $cmds .= 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dolorum facilis maxime quia, nesciunt, voluptatem excepturi voluptas aut asperiores molestiae, eveniet dolorem nulla neque impedit aperiam possimus voluptatibus quisquam itaque. ';
+            // $cmds .= 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dolorum facilis maxime quia, nesciunt, voluptatem excepturi voluptas aut asperiores molestiae, eveniet dolorem nulla neque impedit aperiam possimus voluptatibus quisquam itaque. ';
+            // $cmds .= 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dolorum facilis maxime quia, nesciunt, voluptatem excepturi voluptas aut asperiores molestiae, eveniet dolorem nulla neque impedit aperiam possimus voluptatibus quisquam itaque. ';
+            // $cmds .= 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dolorum facilis maxime quia, nesciunt, voluptatem excepturi voluptas aut asperiores molestiae, eveniet dolorem nulla neque impedit aperiam possimus voluptatibus quisquam itaque. ';
+            // $cmds .= 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dolorum facilis maxime quia, nesciunt, voluptatem excepturi voluptas aut asperiores molestiae, eveniet dolorem nulla neque impedit aperiam possimus voluptatibus quisquam itaque. ';
+            // $cmds .= 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dolorum facilis maxime quia, nesciunt, voluptatem excepturi voluptas aut asperiores molestiae, eveniet dolorem nulla neque impedit aperiam possimus voluptatibus quisquam itaque. ';
+            // $cmds .= 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dolorum facilis maxime quia, nesciunt, voluptatem excepturi voluptas aut asperiores molestiae, eveniet dolorem nulla neque impedit aperiam possimus voluptatibus quisquam itaque. ';
+            // $cmds .= 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dolorum facilis maxime quia, nesciunt, voluptatem excepturi voluptas aut asperiores molestiae, eveniet dolorem nulla neque impedit aperiam possimus voluptatibus quisquam itaque. ';
+            // $cmds .= 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dolorum facilis maxime quia, nesciunt, voluptatem excepturi voluptas aut asperiores molestiae, eveniet dolorem nulla neque impedit aperiam possimus voluptatibus quisquam itaque. ';
+            // $cmds .= 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dolorum facilis maxime quia, nesciunt, voluptatem excepturi voluptas aut asperiores molestiae, eveniet dolorem nulla neque impedit aperiam possimus voluptatibus quisquam itaque. ';
+            // $cmds .= 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dolorum facilis maxime quia, nesciunt, voluptatem excepturi voluptas aut asperiores molestiae, eveniet dolorem nulla neque impedit aperiam possimus voluptatibus quisquam itaque. ';
+            // $cmds .= 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dolorum facilis maxime quia, nesciunt, voluptatem excepturi voluptas aut asperiores molestiae, eveniet dolorem nulla neque impedit aperiam possimus voluptatibus quisquam itaque. ';
+            // $cmds .= 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dolorum facilis maxime quia, nesciunt, voluptatem excepturi voluptas aut asperiores molestiae, eveniet dolorem nulla neque impedit aperiam possimus voluptatibus quisquam itaque. ';
+            // $cmds .= 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dolorum facilis maxime quia, nesciunt, voluptatem excepturi voluptas aut asperiores molestiae, eveniet dolorem nulla neque impedit aperiam possimus voluptatibus quisquam itaque. ';
+            // $cmds .= 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dolorum facilis maxime quia, nesciunt, voluptatem excepturi voluptas aut asperiores molestiae, eveniet dolorem nulla neque impedit aperiam possimus voluptatibus quisquam itaque. ';
+            // $cmds .= 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dolorum facilis maxime quia, nesciunt, voluptatem excepturi voluptas aut asperiores molestiae, eveniet dolorem nulla neque impedit aperiam possimus voluptatibus quisquam itaque. ';
+            // $cmds .= 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dolorum facilis maxime quia, nesciunt, voluptatem excepturi voluptas aut asperiores molestiae, eveniet dolorem nulla neque impedit aperiam possimus voluptatibus quisquam itaque. ';
+            // $cmds .= 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dolorum facilis maxime quia, nesciunt, voluptatem excepturi voluptas aut asperiores molestiae, eveniet dolorem nulla neque impedit aperiam possimus voluptatibus quisquam itaque. ';
+            // $cmds .= 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dolorum facilis maxime quia, nesciunt, voluptatem excepturi voluptas aut asperiores molestiae, eveniet dolorem nulla neque impedit aperiam possimus voluptatibus quisquam itaque. ';
+            // $cmds .= 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dolorum facilis maxime quia, nesciunt, voluptatem excepturi voluptas aut asperiores molestiae, eveniet dolorem nulla neque impedit aperiam possimus voluptatibus quisquam itaque. ';
+            // $cmds .= 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dolorum facilis maxime quia, nesciunt, voluptatem excepturi voluptas aut asperiores molestiae, eveniet dolorem nulla neque impedit aperiam possimus voluptatibus quisquam itaque. ';
+
+            // $cmds .= self::ESC_INITIALIZE;
+
+
 
             // SAMPLE FROM OFFICIAL WEBSITE.
             //Create ESC/POS commands for sample receipt
